d3c11d923a7229eec978dc63c62801047e54606c
Chaitanya Deshpande <cdeshpa2@gmail.com>
2020-08-24 23:25:14 -0400

    added ejs modules pages

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 const app = express();
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000");
+  console.log("Listening on port 3000");
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
+  const day = date.getDate();
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", (req, res) => {
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
